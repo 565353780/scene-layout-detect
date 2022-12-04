39d3ea9c55de24be547b330e3088b0608df9c0c9
chLi <565353780@qq.com>
2022-12-04 23:21:57 +0800

    create get in range angle idx algo

--- scene_layout_detect/Method/angle.py
+++ scene_layout_detect/Method/angle.py
@@ -13,3 +13,11 @@ def getPointsAngle(start_point, point_array, return_angle=True):
 
     angle_array = rad_array * 180.0 / np.pi
     return angle_array
+
+
+def getInRangeAngleIdx(angle_array, angle_min, angle_max):
+    higher_angle_mask = angle_array >= angle_min
+    lower_angle_mask = angle_array <= angle_max
+    in_range_angle_mask = higher_angle_mask & lower_angle_mask
+    in_range_angle_idx = np.where(in_range_angle_mask == True)[0]
+    return in_range_angle_idx
