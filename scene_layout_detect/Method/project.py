a019b499baa2cdb8e6c26fc78509aebb087d05df
chLi <565353780@qq.com>
2022-12-05 15:27:04 +0800

    set dim always be 3

--- scene_layout_detect/Method/project.py
+++ scene_layout_detect/Method/project.py
@@ -5,15 +5,9 @@ import numpy as np
 from copy import deepcopy
 
 
-def getProjectPoints(point_array, dim=2):
-    assert dim in [2, 3]
-
+def getProjectPoints(point_array):
     points = point_array[np.where(point_array[:, 0] != float("inf"))[0]]
     project_points = deepcopy(points)
-
-    if dim == 2:
-        project_points = project_points[:, :2]
-        return project_points
 
     project_points[:, 2] = 0
     return project_points
