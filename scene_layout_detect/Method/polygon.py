457bca559dd4c25dea557be6ee49fbbb7084337d
chLi <565353780@qq.com>
2022-12-05 15:26:57 +0800

    finish polygon generate

--- scene_layout_detect/Method/polygon.py
+++ scene_layout_detect/Method/polygon.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 from copy import deepcopy
 
 import numpy as np
@@ -15,8 +14,11 @@ def getPolygon(camera_point, point_array, delta_angle):
 
     points = getProjectPoints(point_array)
 
-    copy_camera_point = deepcopy(camera_point).reshape(3)[:2]
+    copy_camera_point = deepcopy(camera_point).reshape(3)
+    copy_camera_point[2] = 0.0
     angle_array = getPointsAngle(copy_camera_point, points)
+
+    point_list.append(copy_camera_point)
 
     angle_num = int(360.0 / delta_angle)
     delta_angle = 360.0 / angle_num
@@ -27,6 +29,20 @@ def getPolygon(camera_point, point_array, delta_angle):
                                                 angle_max)
         if in_range_point_idx.shape[0] == 0:
             continue
+
         angle_mean = (angle_min + angle_max) / 2.0
         rad_mean = angle_mean * np.pi / 180.0
-    return point_list
+
+        diff_array = points[in_range_point_idx] - copy_camera_point
+        dist_array = np.linalg.norm(diff_array, ord=2, axis=1)
+        max_dist = np.max(dist_array)
+
+        max_dist_point = np.array([
+            copy_camera_point[0] + np.cos(rad_mean) * max_dist,
+            copy_camera_point[1] + np.sin(rad_mean) * max_dist, 0.0
+        ])
+
+        point_list.append(max_dist_point)
+
+    polygon = np.array(point_list)
+    return polygon
--- scene_layout_detect/Method/render.py
+++ scene_layout_detect/Method/render.py
@@ -8,12 +8,13 @@ from copy import deepcopy
 from scannet_sim_manage.Method.render import render
 
 from scene_layout_detect.Method.project import getProjectPoints
+from scene_layout_detect.Method.polygon import getPolygon
 
 
-def renderProjectPoints(camera_point, point_array):
+def getProjectPCD(camera_point, point_array):
     copy_camera_point = deepcopy(camera_point).reshape(1, 3)
     copy_camera_point[0][2] = 0.0
-    points = getProjectPoints(point_array, 3)
+    points = getProjectPoints(point_array)
     points = np.vstack((points, copy_camera_point))
 
     colors = np.zeros_like(points)
@@ -23,6 +24,30 @@ def renderProjectPoints(camera_point, point_array):
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(points)
     pcd.colors = o3d.utility.Vector3dVector(colors)
+    return pcd
 
+
+def getPolygonPCD(camera_point, point_array, delta_angle):
+    points = getPolygon(camera_point, point_array, delta_angle)
+
+    colors = np.zeros_like(points)
+    colors[:, 2] = 1.0
+
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points)
+    pcd.colors = o3d.utility.Vector3dVector(colors)
+    return pcd
+
+
+def renderProjectPoints(camera_point, point_array):
+    pcd = getProjectPCD(camera_point, point_array)
     render([pcd])
+    return True
+
+
+def renderPolygon(camera_point, point_array, delta_angle):
+    pcd = getProjectPCD(camera_point, point_array)
+    polygon_pcd = getPolygonPCD(camera_point, point_array, delta_angle)
+
+    render([pcd, polygon_pcd])
     return True
