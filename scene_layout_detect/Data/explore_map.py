638939760ef8dc86df24b7986ecf49ec0228dcc2
chLi <565353780@qq.com>
2022-12-09 21:41:24 +0800

    finish update free area by polygon

--- scene_layout_detect/Data/explore_map.py
+++ scene_layout_detect/Data/explore_map.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from copy import deepcopy
+from math import ceil
+
 import cv2
 import numpy as np
 import open3d as o3d
-from math import ceil
-from copy import deepcopy
 
 from scene_layout_detect.Config.color import (FREE_COLOR, OBSTACLE_COLOR,
                                               UNKNOWN_COLOR)
@@ -14,10 +15,15 @@ from scene_layout_detect.Method.render import drawMeshList
 
 class ExploreMap(object):
 
-    def __init__(self, unit_size=0.1, free_width=5, floor_height_max=0.1):
+    def __init__(self,
+                 unit_size=0.1,
+                 free_width=5,
+                 floor_height_max=0.1,
+                 agent_height_max=2):
         self.unit_size = unit_size
         self.free_width = free_width
         self.floor_height_max = floor_height_max
+        self.agent_height_max = agent_height_max
 
         self.floor_z_value = None
         self.map_start_point = None
@@ -29,19 +35,13 @@ class ExploreMap(object):
         self.map = None
         return True
 
-    def updateMap(self, point_array):
-        if point_array.shape[0] == 0:
+    def updateMap(self, polygon):
+        if polygon.shape[0] == 0:
             return True
-
-        min_z_value = np.min(point_array[:, 2])
-        if self.floor_z_value is None:
-            self.floor_z_value = min_z_value
-        else:
-            self.floor_z_value = min(self.floor_z_value, min_z_value)
 
         scale = 1.0 / self.unit_size
 
-        points = deepcopy(point_array)
+        points = deepcopy(polygon)
         points = points * scale
 
         min_point = np.min(points, axis=0)
@@ -91,6 +91,26 @@ class ExploreMap(object):
         self.map = new_map
         return True
 
+    def updateFree(self, polygon):
+        self.updateMap(polygon)
+
+        points = []
+        for point in polygon:
+            pixel = self.getPixelFromPoint(point)
+            points.append(pixel)
+        points = np.array(points, dtype=int)
+
+        new_map = np.zeros_like(self.map, dtype=np.uint8)
+        pts = points[..., ::-1]
+        cv2.fillPoly(new_map, [pts], 255)
+
+        not_obstacle_mask = self.map != OBSTACLE_COLOR
+        free_mask = new_map == 255
+        update_free_mask = free_mask & not_obstacle_mask
+        update_free_idx = np.where(update_free_mask == True)
+        self.map[update_free_idx] = FREE_COLOR
+        return True
+
     def getPixelFromPoint(self, point):
         scale_point = point / self.unit_size
         diff = scale_point - self.map_start_point
@@ -105,44 +125,35 @@ class ExploreMap(object):
             dtype=float)
         return point
 
-    def updateObstacle(self, point_array, paint_width=3):
+    def updateObstacle(self, point_array, paint_radius=0.1):
+        paint_pixel_length = ceil(paint_radius / self.unit_size)
+
         for point in point_array:
             z_value = point[2]
             pixel = self.getPixelFromPoint(point)
-            x_start_pixel = max(0, pixel[0] - paint_width)
-            x_end_pixel = min(self.map.shape[0], pixel[0] + paint_width)
-            y_start_pixel = max(0, pixel[1] - paint_width)
-            y_end_pixel = min(self.map.shape[1], pixel[1] + paint_width)
-            if z_value - self.floor_z_value < self.floor_height_max:
+            x_start_pixel = max(0, pixel[0] - paint_pixel_length)
+            x_end_pixel = min(self.map.shape[0], pixel[0] + paint_pixel_length)
+            y_start_pixel = max(0, pixel[1] - paint_pixel_length)
+            y_end_pixel = min(self.map.shape[1], pixel[1] + paint_pixel_length)
+            point_height = z_value - self.floor_z_value
+            if point_height < self.floor_height_max or \
+                    point_height > self.agent_height_max:
                 continue
             self.map[x_start_pixel:x_end_pixel,
                      y_start_pixel:y_end_pixel] = OBSTACLE_COLOR
         return True
 
-    def updateFree(self, point_array, paint_width=3):
-        for point in point_array:
-            z_value = point[2]
-            pixel = self.getPixelFromPoint(point)
-            x_start_pixel = max(0, pixel[0] - paint_width)
-            x_end_pixel = min(self.map.shape[0], pixel[0] + paint_width)
-            y_start_pixel = max(0, pixel[1] - paint_width)
-            y_end_pixel = min(self.map.shape[1], pixel[1] + paint_width)
-            if z_value - self.floor_z_value < self.floor_height_max:
-                block = self.map[x_start_pixel:x_end_pixel,
-                                 y_start_pixel:y_end_pixel]
-                not_obstacle_idx = np.where(block != OBSTACLE_COLOR)
-                block[not_obstacle_idx] = FREE_COLOR
-                self.map[x_start_pixel:x_end_pixel,
-                         y_start_pixel:y_end_pixel] = block
-        return True
-
-    def addPoints(self, point_array, paint_width=3, render=True):
+    def addPoints(self, point_array, paint_radius=0.1, render=False):
         bound_points = point_array[np.where(
             point_array[:, 0] != float("inf"))[0]]
 
-        self.updateMap(bound_points)
-        self.updateObstacle(bound_points, paint_width)
-        self.updateFree(bound_points, paint_width)
+        min_z_value = np.min(bound_points[:, 2])
+        if self.floor_z_value is None:
+            self.floor_z_value = min_z_value
+        else:
+            self.floor_z_value = min(self.floor_z_value, min_z_value)
+
+        self.updateObstacle(bound_points, paint_radius)
 
         if render:
             pcd = o3d.geometry.PointCloud()
