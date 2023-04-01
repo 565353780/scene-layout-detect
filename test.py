19712d8c6605094c57e204dafeb5e2af2d4ea022
chLi <565353780@qq.com>
2023-04-01 20:42:53 +0800

    remove unused import

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ from scene_layout_detect.Test.fit import test as test_fit_line
 from scene_layout_detect.Test.polyline import test as test_polyline
 
 if __name__ == "__main__":
-    test_fit_line()
+    #  test_fit_line()
     test_polyline()
