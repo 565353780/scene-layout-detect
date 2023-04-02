497c899d12b7bade4266490986f0c32fce611ce5
chLi <565353780@qq.com>
2023-04-02 21:06:53 +0800

    finish polyline renderer module

--- demo.py
+++ demo.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from scene_layout_detect.Demo.explore_point_extractor import \
-    demo as demo_extract_explore_point
+from scene_layout_detect.Demo.explore_point_extractor import demo as demo_extract_explore_point
+from scene_layout_detect.Demo.polyline_renderer import demo as demo_render_polyline
 
 if __name__ == "__main__":
-    demo_extract_explore_point()
+    #  demo_extract_explore_point()
+    demo_render_polyline()
