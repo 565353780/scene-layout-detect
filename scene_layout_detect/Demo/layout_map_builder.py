50e3d95ab5be6074e56202571b3d34b7043be116
chLi <565353780@qq.com>
2023-04-04 16:04:37 +0800

    add layout map builder demo func

---
+++ scene_layout_detect/Demo/layout_map_builder.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.append('../scannet-sim-manage')
+
+from scene_layout_detect.Module.layout_map_builder import LayoutMapBuilder
+
+
+def demo():
+    delta_angle = 2
+    unit_size = 0.01
+    free_width = 50
+    wall_height = 3
+    explore_paint_radius = 0.1
+    min_explore_point_dist = 5
+    render = True
+
+    layout_map_builder = LayoutMapBuilder(delta_angle, unit_size, free_width)
+
+    layout_map_builder.addPoints([0, 0, 0], [
+        [100, 0, 0],
+        [100, 100, 0],
+        [0, 100, 0],
+    ], explore_paint_radius, render)
+    layout_map_builder.updateLayoutMesh(wall_height, render)
+    layout_map_builder.updateExplorePointIdx(min_explore_point_dist, render)
+    return True
